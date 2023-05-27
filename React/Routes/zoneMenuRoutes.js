b64843c8f274b2b9fd22ec65c6a8b8d3b354ac9d
Nathan Ro <119866864+nathanro09@users.noreply.github.com>
2023-05-26 17:51:32 -0700

    Update zoneMenuRoutes.js

--- React/Routes/zoneMenuRoutes.js
+++ React/Routes/zoneMenuRoutes.js
@@ -1,17 +1,10 @@
 import { lazy } from "react";
 
-const TicTacToeZoneMenu = lazy(() =>
-  import("../support/tictactoezone/tictactoe/TicTacToeZoneMenu")
-);
 const KArcadeMenu = lazy(() =>
   import("../training/languages/korean/KArcadeMenu")
 );
 
 const zoneMenus = [
-    {
-        id: 7,
-        element: TicTacToeZoneMenu,
-    },
     {
         id: 10,
         element: KArcadeMenu,
